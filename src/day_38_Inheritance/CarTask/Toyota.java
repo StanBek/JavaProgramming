6eb06a6e6e85373b0fb185ea4cc95287b3484b19
arstanbektemirov <abektemirov82@gmail.com>
2022-01-13 10:45:44 -0500

    package day_38_Inheritance.CarTask;

---
+++ src/day_38_Inheritance/CarTask/Toyota.java
@@ -0,0 +1,8 @@
+package day_38_Inheritance.CarTask;
+
+public class Toyota extends Car {
+
+    public Toyota(String brand, String model, int year, double price, String color, int miles) {
+        super(brand, model, year, price, color, miles);
+    }
+}
