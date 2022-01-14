fd374958422fd4654969115d9d94fc40660b107e
arstanbektemirov <abektemirov82@gmail.com>
2022-01-14 11:58:03 -0500

    day_39_recap;

--- src/Day_39_Recap/ShapeTask/Circle.java
+++ src/Day_39_Recap/ShapeTask/Circle.java
@@ -4,22 +4,40 @@ public class Circle extends Shape {
 
     private double radius;
 
-    public static double pi=3.14;
+    public static double pi = 3.14;
 
     public double getRadius() {
         return radius;
     }
 
     public void setRadius(double radius) {
-        if(radius<=0) {
+        if (radius <= 0) {
             System.err.println("Invalid radius: " + radius);
             System.exit(1);
         }
         this.radius = radius;
     }
 
-    public Circle( double radius) {
+    public Circle(double radius) {
         super("Circle");
         setRadius(radius);
+    }
+
+    public double area() {
+        return radius * radius * pi;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * radius * pi;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                ", area='" + area() + '\'' +
+                ", perimeter='" + perimeter() + '\'' +
+                '}';
     }
 }
