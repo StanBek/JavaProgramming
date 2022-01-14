21696a6310ff44ed97a8083c56cfdc6ee0a073f8
arstanbektemirov <abektemirov82@gmail.com>
2022-01-14 11:58:23 -0500

    day_39_recap;

---
+++ src/Day_39_Recap/MethodOverloading.java
@@ -0,0 +1,16 @@
+package Day_39_Recap;
+
+public class MethodOverloading {
+
+    private int side;
+
+    public MethodOverloading(int side) {
+        this.side = side;
+    }
+
+
+
+    private int method1(int a){
+        return a;
+    }
+}
