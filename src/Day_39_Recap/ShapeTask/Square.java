e788b4149db3775c2ada9b3785ad2a29ef887673
arstanbektemirov <abektemirov82@gmail.com>
2022-01-14 11:36:15 -0500

    day_39_recap;

--- src/Day_39_Recap/ShapeTask/Square.java
+++ src/Day_39_Recap/ShapeTask/Square.java
@@ -1,6 +1,44 @@
 package Day_39_Recap.ShapeTask;
 
-public class Square {
+public class Square extends Shape {
 
-    private int side;
+    private double side;
+
+    public Square(String name, double side) {
+        super("Square");
+        setSide(side);
+    }
+
+    public Square(String name) {
+        super(name);
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        if(side<=0) {
+            System.err.println("Invalid side: " + side);
+            System.exit(1);
+        }
+        this.side = side;
+    }
+
+    public double area(){
+        return side*side;
+    }
+
+    public double perimeter(){
+        return side*4;
+    }
+
+    @Override
+    public String toString() {
+        return "Square{" +
+                "side= " + side +
+                ", area= " + area() +'\''+
+                ", perimeter= " + perimeter() +'\''+
+                '}';
+    }
 }
