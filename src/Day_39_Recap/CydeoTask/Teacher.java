d4dda7c0fcb122ae0edf02ef77b8c319fee2bff3
arstanbektemirov <abektemirov82@gmail.com>
2022-01-14 16:09:45 -0500

    day_39_recap;

---
+++ src/Day_39_Recap/CydeoTask/Teacher.java
@@ -0,0 +1,13 @@
+package Day_39_Recap.CydeoTask;
+
+public class Teacher extends Employee{
+
+    public Teacher(String name, int age, char gender, int employeeID, String jobTitle, double salary) {
+        super(name, age, gender, employeeID, jobTitle, salary);
+    }
+
+    @Override
+    public void work() {
+        System.out.println(getName()+" "+getJobTitle()+" is teaching");;
+    }
+}
