9f4cd1d34fdd6f8b85ea94d66f0959e15fb87aba
arstanbektemirov <abektemirov82@gmail.com>
2022-01-27 12:44:10 -0500

    day_42_Exceptions continue

--- src/Day_42_Exceptions/ThrowKeyword2.java
+++ src/Day_42_Exceptions/ThrowKeyword2.java
@@ -1,10 +1,15 @@
 package Day_42_Exceptions;
 
+import Day_31_Constractors.Rectangle;
+
 import java.util.NoSuchElementException;
 
 public class ThrowKeyword2 {
     public static void main(String[] args) {
 
-        throw new NoSuchElementException("There is no such element as break");
+       throw new NoSuchElementException("There is no such element as break");
+
+        //throw new InterruptedException();
+
     }
 }
