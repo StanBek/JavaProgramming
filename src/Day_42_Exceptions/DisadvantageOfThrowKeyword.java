6ca143313208bdb1a0e7ba4a179b1b9120df10fb
arstanbektemirov <abektemirov82@gmail.com>
2022-01-27 11:24:39 -0500

    day_42_Exceptions continue

---
+++ src/Day_42_Exceptions/DisadvantageOfThrowKeyword.java
@@ -0,0 +1,22 @@
+package Day_42_Exceptions;
+
+public class DisadvantageOfThrowKeyword {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.out.println("Hello");
+        sleep(2.5);//throws keyword
+        System.out.println("Hello world");
+
+        System.out.println("=====================");
+
+        System.out.println("Hello");
+        MorningWorkOut.sleep(2.5);// try and catch
+        System.out.println("World");
+    }
+
+    public static void sleep(double second) throws InterruptedException {
+
+            Thread.sleep((long) (second*1000));
+
+    }
+}
