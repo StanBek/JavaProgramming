55109d4b0fa7bf1b822f7b9e20eca64bb047eb6c
arstanbektemirov <abektemirov82@gmail.com>
2022-01-27 12:37:54 -0500

    day_42_Exceptions continue

--- src/Day_42_Exceptions/ThrowKeyword2.java
+++ src/Day_42_Exceptions/ThrowKeyword2.java
@@ -1,4 +1,10 @@
 package Day_42_Exceptions;
 
+import java.util.NoSuchElementException;
+
 public class ThrowKeyword2 {
+    public static void main(String[] args) {
+
+        throw new NoSuchElementException("There is no such element as break");
+    }
 }
