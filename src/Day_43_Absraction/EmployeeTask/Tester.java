82f3e8e519686fedc7cc5c87915d4ddb15b7b799
arstanbektemirov <abektemirov82@gmail.com>
2022-01-31 16:36:18 -0500

    day_43_Abstraction

---
+++ src/Day_43_Absraction/EmployeeTask/Tester.java
@@ -0,0 +1,21 @@
+package Day_43_Absraction.EmployeeTask;
+
+public class Tester extends Employee{
+
+    public Tester(String name, int age, int id, char gender, String jobTitle) {
+        super(name, age, id, gender, jobTitle);
+    }
+
+    public void work() {
+        System.out.println(getName()+" is testing the application");
+    }
+
+
+    public void sleep() {
+        System.out.println(getName()+" sleeps 8 hours");
+    }
+
+    public void bugReport(){
+        System.out.println(getName()+" is creating bug reports");
+    }
+}
