888e12eb5a0a981491691fff20d5fbd80198ee90
arstanbektemirov <abektemirov82@gmail.com>
2022-01-31 14:29:25 -0500

    day_43_Abstraction

---
+++ src/Day_43_Absraction/car/Honda.java
@@ -0,0 +1,12 @@
+package Day_43_Absraction.car;
+
+public class Honda extends Car{
+    public Honda(String brand, String model, String color, int year, double price) {
+        super(brand, model, color, year, price);
+    }
+
+    @Override
+    public void start() {
+
+    }
+}
