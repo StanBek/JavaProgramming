3361cf9017f88ab9018f005ce3cf21bff7b2e79d
arstanbektemirov <abektemirov82@gmail.com>
2022-01-31 16:38:51 -0500

    day_43_Abstraction

--- src/Day_43_Absraction/EmployeeTask/Developer.java
+++ src/Day_43_Absraction/EmployeeTask/Developer.java
@@ -1,4 +1,15 @@
 package Day_43_Absraction.EmployeeTask;
 
-public class Developer extends {
+public class Developer extends Employee{
+    public Developer(String name, int age, int id, char gender, String jobTitle) {
+        super(name, age, id, gender, jobTitle);
+    }
+
+    public void work() {
+        System.out.println(getName()+" creating application");
+    }
+
+    public void sleep() {
+        System.out.println(getName()+" sleeps 7 hours");
+    }
 }
